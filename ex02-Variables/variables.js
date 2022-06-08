7ab6ea18cf9c219d6f3535baec58fa3eefdb03b8
Augusto Brandão <37248840+AugustoBrandao@users.noreply.github.com>
2022-06-07 22:12:09 -0300

    variables

---
+++ ex02-Variables/variables.js
@@ -0,0 +1,19 @@
+// Comentário de uma linha
+/* Comentário de várias linhas */
+
+/* CONVERSÃO DE DADOS:
+        - Number.parseFloat()
+        - Number.parseInt()
+        - String() */
+
+
+/*  MANIPULAÇÃO DE CARACTERES:
+        - variavel.length - quantos caracteres tem
+        - variavel.toUpperCase()
+        - variavel.toLowerCase() */
+
+
+/*  MANIPULAÇÃO DE NÚMEROS DECIMAIS:
+        - variavel.toFixed(2) - apenas com duas casas decimais
+        - variavel.toFixed(2).replace('.',',') - trocar o ponto por vírgula
+        - veriavel.toLocaleString('pt-BR', {style:'currency', currency: 'BRL'}) - moeda em real */
