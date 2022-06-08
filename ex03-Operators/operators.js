bd6fef34b579479903fe8990668a11a124401c09
Augusto Brandão <37248840+AugustoBrandao@users.noreply.github.com>
2022-06-07 22:56:48 -0300

    operators

---
+++ ex03-Operators/operators.js
@@ -0,0 +1,20 @@
+/*
+    ORDEM DE PRECEDÊNCIA DOS OPERADORES ARITMÉTICOS
+    1- Parênteses ()
+    2- Potenciação **
+    3- Multiplicação * , divisão / , módulo % 
+    4- Soma + e Subtração -
+*/
+
+/* ORDEM DE PRECEDÊNCIA DE OPERADORES
+    - Aritméticos: () ** * / %
+    - Relacionais: > < >= <=  (esquerda para direita)
+    - !  not
+    - && and 
+    - || or
+*/
+
+/*
+    OPERADOR TERNÁRIO:
+     - teste ? true : false
+*/
