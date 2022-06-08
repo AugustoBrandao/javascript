c312ed85c962264f070f2a459bc6954041771641
Augusto Brandão <37248840+AugustoBrandao@users.noreply.github.com>
2022-06-08 11:35:49 -0300

    conditions

---
+++ ex05-If-Else/index.js
@@ -0,0 +1,8 @@
+var velocidade = 120
+
+if(velocidade>100){ 
+    console.log(`Velocidade: ${velocidade} | Status: Multado`)
+}
+else{ 
+    console.log(`Velocidade: ${velocidade} | Status: Normal`)
+}
