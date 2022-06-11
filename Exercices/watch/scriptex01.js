d87d1061e7db0ed522465df3bfb2cc84ab173f6e
Augusto Brandão <37248840+AugustoBrandao@users.noreply.github.com>
2022-06-10 23:08:25 -0300

    .

--- Exercices/watch/scriptex01.js
+++ Exercices/watch/scriptex01.js
@@ -5,19 +5,27 @@ var month = window.document.getElementById('month')
 var year = window.document.getElementById('year')  
 var hour = window.document.getElementById('hour')
 var minutes = window.document.getElementById('minutes')
+var seconds = window.document.getElementById('seconds')
 
 day.innerHTML = agora.getDate()+'/ '
 month.innerHTML = (Number(agora.getMonth())+1)+'/'
 year.innerHTML = agora.getFullYear()
 hour.innerHTML = agora.getHours()+':' 
 
-if(agora.getMinutes() < 10){
-    minutes.innerHTML = '0'+agora.getMinutes()
+if(agora.getSeconds() < 10 ){
+    seconds.innerHTML = '0'+agora.getSeconds()
 }else{
-    minutes.innerHTML = agora.getMinutes()
+    seconds.innerHTML = agora.getSeconds()
 }
+
+if(agora.getMinutes() < 10){
+    minutes.innerHTML = '0'+agora.getMinutes()+':'
+}else{
+    minutes.innerHTML = agora.getMinutes()+':'
+}
+
 
 //Reload the page every one minute
 window.setTimeout( function() {
     window.location.reload();
-  }, 60000);
+}, 900);
