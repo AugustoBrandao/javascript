5042d445e489bc3bc7342c19077f838d9925b459
Augusto Brandão <37248840+AugustoBrandao@users.noreply.github.com>
2022-10-24 16:28:59 -0300

    Commit dia 24/10/2022

--- ex08-Functions/arrowfunctions.js
+++ ex08-Functions/arrowfunctions.js
@@ -1,1 +1,16 @@
-//São Sempre funções anônimas
+//Função Anônima
+const funcao = function (a,b){ return a + b; }
+
+//Arrow Function - 1 linha de código
+const arrowFunction = (a,b) => a + b; //um parâmetro
+const soma = (num1 , num2) => num1 + num2;//dois parâmetros
+
+
+//Arrow Function - com duas ou mais linhas de código
+const somaNumerosPequenos = (num1, num2) =>{
+    if(num1 > 10 || num2 > 10){
+        return "Somente números de 0 à 10"
+    }else{
+        return num1 + num2;
+    }
+}
--- ex08-Functions/function02.js
+++ ex08-Functions/function02.js
@@ -4,6 +4,7 @@ function soma(n1=0, n2=0){
 
 console.log(soma(2,5))
 
+
 let v = function(x){ return x*2 }
 console.log(v())
 
