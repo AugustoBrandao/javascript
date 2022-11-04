face0dc7193be9e519cbfd609aba34534ec1a9fe
Augusto Brandão <37248840+AugustoBrandao@users.noreply.github.com>
2022-11-04 07:20:15 -0300

    objects

--- ex09-Objects/object.js
+++
@@ -1,7 +0,0 @@
- //OBJETOS - Formado por chave e valor de diferentes tipos
- const objPessoa = {
-    nome: "Augusto",
-    idade: 23,
-    cpf: "456.039.238-22",
-    email: "augustobrandao.99@outlook.com"
- }
